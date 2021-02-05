a06819ca253014155fda2bc0b0e5a05f7bcc94ba
Invader Zim <pwstern@gmail.com>
2021-02-04 21:28:53 -0800

    Initial setup.

--- RPCalculator/CalculatorViewController.swift
+++ RPCalculator/CalculatorViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  CalculatorViewController.swift
 //  RPCalculator
 //
 //  Created by Phil Stern on 2/4/21.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CalculatorViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
 
 }
 
