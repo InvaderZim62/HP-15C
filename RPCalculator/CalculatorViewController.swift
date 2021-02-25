359cdcc061df574762eaef6f4404e5a5d10af124
Invader Zim <pwstern@gmail.com>
2021-02-25 14:07:24 -0800

    Moved enum DisplayFormat into its own file.

--- RPCalculator/CalculatorViewController.swift
+++ RPCalculator/CalculatorViewController.swift
@@ -37,52 +37,6 @@
 import UIKit
 import AVFoundation  // needed for AVAudioPlayer
 
-enum DisplayFormat: Codable {  // pws: move to its own file?
-    case fixed(Int)  // (decimal places)
-    case scientific(Int)  // (decimal places)
-    case engineering(Int)  // (additional digits) similar to scientific, except exponent is always a multiple of three
-    
-    var string: String {  // usage: String(format: displayFormat.string, number)
-        switch self {
-        case .fixed(let decimalPlaces):
-            return "%.\(decimalPlaces)f"
-        case .scientific(let decimalPlaces):
-            return "%.\(decimalPlaces)e"
-        case .engineering(let aditionalDigits):
-            return "%.\(aditionalDigits)e"  // engineering format can't be done through string alone (see runAndUpdateInterface)
-        }
-    }
-    
-    // MARK: - Codable
-    
-    private enum CodingKeys: String, CodingKey { case fixed, scientific, engineering }
-    
-    init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        if let value = try? container.decode(Int.self, forKey: .fixed) {
-            self = .fixed(value)
-        } else if let value = try? container.decode(Int.self, forKey: .scientific) {
-            self = .fixed(value)
-        } else if let value = try? container.decode(Int.self, forKey: .engineering) {
-            self = .fixed(value)
-        } else {
-            throw DecodingError.dataCorrupted(DecodingError.Context(codingPath: container.codingPath, debugDescription: "Data doesn't match"))
-        }
-    }
-    
-    func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        switch self {
-        case .fixed(let decimalPlaces):
-            try container.encode(decimalPlaces, forKey: .fixed)
-        case .scientific(let decimalPlaces):
-            try container.encode(decimalPlaces, forKey: .scientific)
-        case .engineering(let aditionalDigits):
-            try container.encode(aditionalDigits, forKey: .engineering)
-        }
-    }
-}
-
 enum Prefix: String {
     case f  // function above button (orange)
     case g  // function below button (blue)
